f6c4f54028fdefc167aaa4f51f5bbd4088fe1bfa
Allison MacLeay <allisonmacleay@ALF4247A.its.caregroup.org>
2014-11-24 14:22:41 -0500

    R files for lab 3 initial commit

---
+++ Lab3/lab3_spinach_monkey.R
@@ -0,0 +1,82 @@
+cat("Finding MLE.Score and Distance\n")
+f<-read.table("lab3_f.txt")
+pam<-read.table("macleay_lab3_pam.txt")
+# just to initialize d as an object
+d<-read.table("macleay_lab3_pam.txt")
+# divide pam matrix by 10,000
+pam1=pam/10000
+# Spinach sequence
+Seq1<-'NGTKESITKLVSDLNSATLEADVDVVVAPPFVYIDQVKSSLTGRVEISAQNCW'
+# Misquito sequence
+Seq2<-'NGDKASIADLCKVLTTGPLNADTEVVVGCPAPYLTLARSQLPDSVCVAAQNCY'
+# 20 Amino Acids
+AA<-'ARNDCQEGHILKMFPSTWYV'
+# initilize S1 and S2 as matrices
+S1<-matrix(c(1:nchar(Seq1)))
+S2<-matrix(c(1:nchar(Seq2)))
+
+# Translate each sequence into an array of number values
+for(i in 1:nchar(Seq1)){
+	ch<-substring(Seq1,i,i)
+	S1[i]<-regexpr(ch,AA)[1]
+}
+for(i in 1:nchar(Seq2)){
+	ch<-substring(Seq2,i,i)
+	S2[i]<-regexpr(ch,AA)[1]
+}
+
+# initialize ProbSum
+ProbSum<-matrix(c(1:150))
+ for(k in 1:150){
+	# pam matrix to the power of k
+	 pamk<-pam1^k
+	 for(i in 1:20){ 
+		for(j in 1:20){
+			# populate d with the pam value divided by
+			# the frequency
+			 d[i,j]<-pamk[i,j]/f[i,1]
+			# Is this a mistake in the lab equation?
+			# below is the equation I have from my notes
+			# last fall.
+			# log( P[i,j] / (Qi * Qj) )
+			# - in R -
+			# d[i,j]<-pamk[i,j]/(f[i,1]*f[j,1])
+		}
+	}
+	# * Normalize the values *
+	# The following line is where I have a problem
+	# log10 is returning negative values
+	D<-10*log10((d+t(d))/2)
+	# matrix + the transform of the matrix divided by 2
+	# normalizes the matrix to ensure it is symmetrical
+	m<-as.matrix(D)
+
+	# * Remove -Inf values returned for log10 of 0
+	m[!is.finite(m)]<-0
+	D<-as.data.frame(m)
+	ProbSum[k]<-0
+	for(i in 1:length(S1)){
+		#cat(c(S1[i],S2[i],i,"D",D[S1[i],S2[i]]),sep="\t")
+		#cat("\n")
+
+		# * for each amino acid return the normalized score
+		# of transitioning from one to the next from the matrix
+		# and sum all values
+		ProbSum[k]<-D[S1[i],S2[i]]+ProbSum[k]
+	}
+	#cat(ProbSum[k])
+	#cat("\n")
+}
+# * find the max likelihood estimation
+MLE.score<-max(ProbSum)
+# * find the PAM distance from the max score
+MLE.PAM.distance<-which(ProbSum==MLE.score)
+
+# * set up x and y to graph values
+x<-matrix(c(1:10))
+y<-x
+for(i in 1:10){
+	x[i]<-i*15
+	y[i]<-ProbSum[i*15]
+}
+cat("MLE.score and MLE.PAM.distance are populated.  Run plot(x,y).\n")
